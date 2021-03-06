0c586c844d0ed5f4277980c553e7af632c0bdbb4
Qinyang Lu <yanglu1037@163.com>
2021-03-06 16:49:50 +0800

    lqy

---
+++ eval.py
@@ -0,0 +1,76 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time: 2021/3/6 4:40 PM
+@Author: Qinyang Lu
+"""
+import torch
+import torch.nn as nn
+
+import numpy as np
+import cv2
+from sklearn import metrics
+
+
+def get_metrics(scores, labels):
+    fpr, tpr, ths = metrics.roc_curve(labels, scores)
+    auc = round(metrics.auc(fpr, tpr), 3)
+
+    rightindex = tpr - fpr
+    index = np.argmax(rightindex)
+    th = ths[index]
+
+    labels_l = np.array(labels)
+    scores_l = np.array(scores)
+    scores_l[scores_l > th] = 1
+    scores_l[scores_l != 1] = 0
+    tp = np.sum(scores_l * labels_l)
+    a = labels_l + scores_l
+    sub = labels_l - scores_l
+
+    tn = np.where(a == 0)[0].shape[0]
+    fp = np.where(sub == -1)[0].shape[0]
+
+    acc = (tp + tn) / labels_l.shape[0]
+    recall = tp / np.sum(labels_l)
+    precision = tp / np.sum(scores_l)
+    specificity = tn / (tn + fp)
+
+    return acc, recall, precision, specificity, auc, th
+
+
+def eval_cls(net, dataloader, gpu):
+    net.eval()
+    labels = []
+    scores = []
+    losses = 0
+
+    criterion = nn.CrossEntropyLoss()
+    i = 0
+    for i, (img, label) in enumerate(dataloader):
+        img = np.asarray(img, np.float32)
+        label = np.asarray([label], np.float32)
+
+        img = torch.from_numpy(np.transpose(img, (2, 0, 1))).unsqueeze(0)
+        img = torch.cat([img, img, img], dim=1)
+        label = torch.from_numpy(label)
+
+        if gpu:
+            img = img.cuda()
+            label = label.cuda()
+
+        logits = net(img)
+        label_flat = label.view(-1)
+
+        loss = criterion(logits, label_flat.long())
+        losses = losses + loss.item()
+
+        predictions_ = torch.softmax(logits, dim=1)
+        scores.append(round(predictions_[0, 1].item(), 4))
+        labels.append(label.item())
+
+    acc, recall, precision, specificity, auc, th = get_metrics(scores, labels)
+
+    net.train()
+
+    return acc, recall, precision, specificity, auc, th, losses / (i+1)
--- train.py
+++ train.py
@@ -4,11 +4,14 @@ import torchvision
 import torch.utils.data as data
 
 from Dataset import ClsDataset
+from eval import eval_cls
 
 import pandas as pd
 
+
 def train_cls(net, epochs, batch_size, lr, gpu, patch_size):
     data_root = "/home/lqy/桌面/sofa_test/"
+    dir_checkpoint = "data/cp/resnet/"
     train_df = pd.DataFrame("./data/train.csv")
     valid_df = pd.DataFrame("./data/valid.csv")
     test_df = pd.DataFrame("./data/test.csv")
@@ -19,11 +22,20 @@ def train_cls(net, epochs, batch_size, lr, gpu, patch_size):
     valid_loader = data.DataLoader(valid_dataset, batch_size=1, shuffle=False)
     test_loader = data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-
     optimizer = torch.optim.Adam(net.parameters(), lr=lr)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
     criterion = nn.CrossEntropyLoss()
     min_loss = 100
+
+    print('''
+    Starting training:
+        Epochs: {}
+        Batch size: {}
+        Learning rate: {}
+        Training size: {}
+        Validation size: {}
+        CUDA: {}
+    '''.format(epochs, batch_size, lr, len(train_df), len(valid_df), str(gpu)))
 
     for epoch in range(epochs):
         print('Starting epoch {}/{}'.format(epoch+1, epochs))
@@ -47,12 +59,33 @@ def train_cls(net, epochs, batch_size, lr, gpu, patch_size):
                 optimizer.step()
             scheduler.step(epoch_loss)
 
+            print('Epoch finished ! Loss: {}'. \
+                  format(epoch_loss / (i + 1)))
 
+            acc, recall, precision, specificity, auc, th, losses = \
+                eval_cls(net, valid_loader, gpu)
+            print('acc: {}, recall: {}, precision: {}, specificity: {}, auc: {}, th: {}, loss: {}'. \
+                  format(acc, recall, precision, specificity, auc, th, losses))
+
+            if losses < min_loss:
+                torch.save(net,
+                           dir_checkpoint + 'CP{}.pt'.format(epoch + 1))
+                print('Checkoutpoint {} saved !\n'.format(epoch + 1))
+                min_loss = losses
+
+            acc, recall, precision, specificity, auc, th, losses = \
+                eval_cls(net, test_loader, gpu)
+            print("test set:")
+            print('acc: {}, recall: {}, precision: {}, specificity: {}, auc: {}, th: {}, loss: {}'. \
+                  format(acc, recall, precision, specificity, auc, th, losses))
 
 
 if __name__ == '__main__':
     pretrained_path = "data/cp/resnet50-19c8e357.pth"
     patch_size = (224, 224)
+    epochs = 100
+    batch_size = 32
+    lr = 0.01
 
     net = torchvision.models.resnet50(pretrained=False)
     net.fc = (2048, 2)
@@ -62,3 +95,9 @@ if __name__ == '__main__':
     if gpu:
         net.cuda()
 
+    train_cls(net=net,
+              epochs=epochs,
+              batch_size=batch_size,
+              lr=lr,
+              gpu=gpu,
+              patch_size=patch_size)
